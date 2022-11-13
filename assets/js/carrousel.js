246f31dfa0ae6655f0f07a6c8941c555b1235ec8
Leandro <98579374+MIIGgames@users.noreply.github.com>
2022-11-13 01:55:35 -0300

    up

--- assets/js/carrousel.js
+++ assets/js/carrousel.js
@@ -1,23 +1,6 @@
-var carusel= [
-	"./img/1.jpg",
-  "./img/2.jpg",
-  "./img/3.jpg",
-]
-
-	var count = 0;
-$("#next").on("click", function(){
-  count++;
-  if (count > carusel.length - 1){
-    count = 0;
-  }
-  $("img").attr("src", carusel[count]);
-});
-
-$("#back").on("click", function(){
-  count--;
-	console.log("working")
-  if (count < 0){
-    count = carusel.length - 1;
-  }
-  $("img").attr("src", carusel[count]);
+$(".slider").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000, //2000ms = 2s;
+  autoplayHoverPause: true,
 });
